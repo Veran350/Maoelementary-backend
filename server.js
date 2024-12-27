331fc3929b9be53b5a03c3cfaef61e407b5448a3
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 09:13:06 +0100

    Update server.js

--- server.js
+++ server.js
@@ -16,4 +16,6 @@ app.use('/api/auth', authRoutes);
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => 
+  console.log(`${process.env.APP_NAME || 'maoelementary'} backend running on port ${PORT}`)
+);

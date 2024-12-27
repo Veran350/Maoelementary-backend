6662aa263bc84a4068d15cc30b3ee30325e7ef66
VERAN-TECH-HUB <veranmaina685@gmail.com>
2024-12-27 09:03:39 +0100

    Create server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const mongoose = require('./config/db');
+const authRoutes = require('./routes/authRoutes');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+// Routes
+app.use('/api/auth', authRoutes);
+
+// Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
